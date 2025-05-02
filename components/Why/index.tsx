5d7afcd6d6737d1580373f89ee25832991dc5a13
Elijah Ndenwa <elijahndenwa19@gmail.com>
2025-05-02 12:10:36 +0100

    Adventist Education Result Management

--- components/Why/index.tsx
+++ components/Why/index.tsx
@@ -8,7 +8,7 @@ const Testimonial = () => {
       <div className="lg:w-1/2 flex justify-center lg:justify-end mt-10 lg:mt-0">
         <div className="relative w-[85%] lg:w-[80%] h-0 pb-[85%]">
           <Image
-            src="/adventist-secretary.jpg"
+            src="/ElenG.png"
             alt="Testimonial Image"
             layout="fill"
             objectFit="cover"
@@ -20,15 +20,16 @@ const Testimonial = () => {
       {/* Text Section */}
       <div className="lg:w-1/2 mt-6 lg:mt-0 text-center flex flex-col items-center">
         <p className="text-lg text-gray-700 italic max-w-xl">
-          Digitalising the education sector empowers schools with tools to
-          streamline processes, enhance transparency, and improve access to
-          information. It fosters efficiency in managing academic records while
-          enabling real-time insights, ultimately raising the quality of
-          education and preparing students for a technology-driven future.
+          True education means more than the pursual of a certain course of
+          study. It means more than a preparation for the life that now is. It
+          has to do with the whole being, and with the whole period of existence
+          possible to man. It is the harmonious development of the physical, the
+          mental, and the spiritual powers. It prepares the student for the joy
+          of service in this world and for the higher joy of wider service in
+          the world to come.
         </p>
-        <p className="mt-4 text-sm text-gray-500">
-          - Pastor Ezekiel Oyinloye, Secretary, <br /> Western Nigeria Union
-          Conference of Seventh-day Adventist Church, Nigeria
+        <p className="mt-4 text-lg text-gray-500">
+          - Education by Ellen G. White <br /> (Education, p. 13)
         </p>
       </div>
     </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    images: {
-      unoptimized: true,
-    },
-    distDir: 'out',
-  };
-  
-  export default nextConfig;
-  
+  output: "export",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
